de4380043a5043d96e77cdf3dc5ec7c2d0ef5147
Keegan <b.keegan12@yahoo.com>
2021-11-23 20:16:05 -0500

    Work on server

--- server/index.js
+++ server/index.js
@@ -2,3 +2,19 @@ import express from 'express';
 import bodyParse from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+const fs = require("fs");
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONN_URL = fs.readFileSync("secret.txt").toString(); // Find a better way to do this for deployment
+const PORT = process.env.PORT || 1337; // Port is 1337
+
+mongoose.connect(CONN_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
