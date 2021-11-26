818c1d95423441e7d03568fc8ef74a14be42ef55
Keegan <b.keegan12@yahoo.com>
2021-11-26 14:29:26 -0500

    Redux setup

--- server/index.js
+++ server/index.js
@@ -8,14 +8,15 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 
+app.use('/posts', postRoutes);
+
+app.use('/posts', postRoutes);
 const CONN_URL = fs.readFileSync("secret.txt").toString(); // Find a better way to do this for deployment
-const PORT = process.env.PORT || 3000; // Port is 3000
+const PORT = process.env.PORT || 5000; // Port is 5000
 
 mongoose.connect(CONN_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
